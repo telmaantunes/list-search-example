0161e0a40912d46bed08c4589b3c744049fe530d
Telma Antunes <telma.olive.antunes@gmail.com>
2024-11-14 15:56:30 +0000

    Protect from xss attacks

--- src/utils/highlightText.ts
+++ src/utils/highlightText.ts
@@ -1,10 +1,15 @@
+import DOMPurify from 'isomorphic-dompurify';
+
 export default function highlightText (text: string, highlight?: string): string {
   if(!highlight) {
     return text;
   }
 
+  // XSS sanitizer
+  const cleanHighlight = DOMPurify.sanitize(highlight);
+
   // Escape special characters in the highlight string
-  const escapedHighlight = highlight.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+  const escapedHighlight = cleanHighlight.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
 
   // Create a case-insensitive regular expression
   const regex = new RegExp(escapedHighlight, 'gi');
