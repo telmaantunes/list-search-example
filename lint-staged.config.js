a0e35838f1fd3a0c491ac8f15a9f5c9c2a3b2a20
Telma Antunes <telma.olive.antunes@gmail.com>
2024-11-14 11:12:07 +0000

    test lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,13 +2,13 @@ export default {
   '*.{js,ts}': [
     'pnpm run lint:script',
     'git add',
-    'pnpm test:unit related --bail 1'
+    'pnpm test:unit related --bail 1 --run'
   ],
   '*.vue': [
     'pnpm run lint:script',
     'pnpm run lint:style',
     'git add',
-    'pnpm test:unit related --bail 1'
+    'pnpm test:unit related --bail 1 --run'
   ],
   '*.scss': [
     'pnpm run lint:style',
